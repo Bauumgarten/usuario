5d140b42e90ea4dd472d1f5de843ef7449ba0a21
RaulBaumgarten <raulbaumgarten@hotmail.com>
2025-08-09 12:15:19 -0300

    Iniciando estudo java

---
+++ src/test/java/com/estudo_raul/java/JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo_raul.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
